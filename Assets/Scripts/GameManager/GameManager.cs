0ed0db6816ebcb60d43f062811840389cf742df3
FoundTexas <rolega01@gmail.com>
2022-07-06 13:58:07 -0500

    GameManager Update

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    static GameManager GM;
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (GM == null)
+        {
+            GM = this;
+        }
+        else if ( GM != null)
+        {
+            if(GM != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
