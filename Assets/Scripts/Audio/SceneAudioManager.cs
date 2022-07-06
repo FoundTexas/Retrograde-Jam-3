6f6e18d69c62059d0dcc7bc7a4e3e998ec6d065d
FoundTexas <rolega01@gmail.com>
2022-07-06 13:50:14 -0500

    Finished Navegation And Score

--- Assets/Scripts/Audio/SceneAudioManager.cs
+++ Assets/Scripts/Audio/SceneAudioManager.cs
@@ -3,40 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-namespace Audio
+[RequireComponent(typeof(AudioSource))]
+public class SceneAudioManager : MonoBehaviour
 {
-    [RequireComponent(typeof(AudioSource))]
-    public class SceneAudioManager : MonoBehaviour
+    static AudioMixer mixer;
+    static AudioSource source;
+
+    void Start()
     {
-        static AudioMixer mixer;
-        static AudioSource source;
-
-        void Start()
+        if (source == null)
         {
-            if (!source)
-            {
-                source = GetComponent<AudioSource>();
-                mixer = source.outputAudioMixerGroup.audioMixer;
-                StartCoroutine(StartFade(2, 1));
-            }
+            source = GetComponent<AudioSource>();
+            mixer = source.outputAudioMixerGroup.audioMixer;
+            StartCoroutine(StartFade(2, 1));
         }
+    }
 
-        public static void PlayPreset(AudioPreset i)
-        {
-            i.SetAudioSource(mixer);
-        }
+    public static void PlayPreset(AudioPreset i)
+    {
+        i.SetAudioSource(mixer);
+    }
 
-        public static IEnumerator StartFade(float duration, float targetVolume)
+    public static IEnumerator StartFade(float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = source.volume;
+        while (currentTime < duration)
         {
-            float currentTime = 0;
-            float start = source.volume;
-            while (currentTime < duration)
-            {
-                currentTime += Time.deltaTime;
-                source.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
-                yield return null;
-            }
-            yield break;
+            currentTime += Time.deltaTime;
+            source.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
         }
+        yield break;
     }
 }
