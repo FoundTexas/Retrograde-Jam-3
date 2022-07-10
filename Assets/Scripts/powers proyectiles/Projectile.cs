a8209808b6c573a9faaa904bb581ca41a274e57b
JEGA27 <nmokey.jega@gmail.com>
2022-07-10 03:24:24 -0500

    Flying Enemy

--- Assets/Scripts/powers proyectiles/Projectile.cs
+++ Assets/Scripts/powers proyectiles/Projectile.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    GameObject target;
+    public float speed;
+    Rigidbody2D projectileRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        projectileRB = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        Vector2 direction = (target.transform.position - transform.position).normalized * speed;
+        projectileRB.velocity = new Vector2(direction.x, direction.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
